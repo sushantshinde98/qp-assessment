cc6bd6aa143fa8fbcae7d08d556253ac5e99d4df
Sushant <sushant.shinde@hungama.com>
2024-02-22 19:31:47 +0530

    index.js updated

--- index.js
+++ index.js
@@ -12,41 +12,6 @@ const port = process.env.PORT || 5000;
 app.use("/v1/", routes);
 
 //sequelize
-// db.sequelize
-//   .authenticate()
-//   .then((res) => {
-//     logger.info("Sequelize authenticated! :", res);
-//     db.sequelize
-//       .query(`CREATE DATABASE IF NOT EXISTS ${config.database.dbname} `)
-//       .then((res) => {
-//         logger.info("Sequelize db query :", res);
-//         db.sequelize.query(`USE ${config.database.dbname} `).then((res) => {
-//           logger.info("Sequelize db query :", res);
-//           db.sequelize
-//             .sync()
-//             .then(() => {
-//               logger.info("Drop and re-sync db.");
-//               db.sequelize
-//                 .query(
-//                   `INSERT INTO USERS (username,password,role,createdAt,updatedAt) VALUES ('sush98','123','user',now(),now()),('sss','1234','admin',now(),now())`
-//                 )
-//                 .then((res) => {
-//                   logger.info("INSERT REC!.");
-//                 })
-//                 .catch((err) => {
-//                   logger.error("INSERT REC ERR!." + err);
-//                 });
-//             })
-//             .catch((err) => {
-//               logger.error("sequelize snc error: ", err);
-//             });
-//         });
-//       });
-//   })
-//   .catch((err) => {
-//     logger.error("Sequelize authenticate error :", err);
-//   });
-
 db.sequelize
   .authenticate()
   .then((res) => {
